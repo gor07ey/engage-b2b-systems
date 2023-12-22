e60c5479f544332c2037351d087e6284ee6ee484
gor07ey <152075976+gor07ey@users.noreply.github.com>
2023-12-22 03:55:02 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,66 @@
+    return data
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+    data = generate_random_data()
+if __name__ == "__main__":
+
+    main()
+import random
+
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    return data
+        print(f"Random Number: {item}")
+    main()
+
+def main():
+import random
+    for item in data:
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+    for item in data:
+        print(f"Random Number: {item}")
+
+
+    main()
+if __name__ == "__main__":
+
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+def main():
+    return data
+    data = generate_random_data()if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    main()
+    return data
+def main():
+    data = generate_random_data()
+import random
+
+
+        print(f"Random Number: {item}")
+    for item in data:
+
+        print(f"Random Number: {item}")
+    main()
+import random
+
+def generate_random_data():
+
+if __name__ == "__main__":
+def main():
+    return data
+    for item in data:
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
